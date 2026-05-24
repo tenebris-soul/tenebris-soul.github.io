f170ad7539c3c056c64a4169bbd2b7986d7e17f7
Jabe <nightlighting_moon@mail.ru>
2026-05-24 17:15:44 +0300

    feat: v4

--- thesaurus-page/src/data/download.js
+++ thesaurus-page/src/data/download.js
@@ -1,3 +1,4 @@
-export const PLUGIN_COMMIT_SHA256 = 'sha256:fb5dd38996d3621de72edf8a29c5be871d3c9f43d08325800c087472f083672d';
-export const PLUGIN_DOWNLOAD_FILENAME = 'thesaurus-plugin-commit.sha256.txt';
-export const PLUGIN_DOWNLOAD_URL = './downloads/thesaurus-plugin-commit.sha256.txt';
+export const PLUGIN_COMMIT_SHA256 =
+  "sha256:fb5dd38996d3621de72edf8a29c5be871d3c9f43d08325800c087472f083672d";
+export const PLUGIN_DOWNLOAD_FILENAME = "v1.0.0-demo.unitypackage";
+export const PLUGIN_DOWNLOAD_URL = "./downloads/v1.0.0-demo.unitypackage";
